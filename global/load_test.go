11cf6ff065fab835cd63c422a9022d6a9a768d9a
Max <shadow.iqka@qq.com>
2021-09-28 16:51:04 +0800

    fix test file bug

--- global/load_test.go
+++ global/load_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yaoapp/xiang/table"
 )
 
 func TestLoad(t *testing.T) {
@@ -43,4 +44,29 @@ func TestLoadAppFS(t *testing.T) {
 			Screens: Conf.RootScreen,
 		})
 	})
+}
+
+func TestTableLoad(t *testing.T) {
+
+	table, has := table.Tables["service"]
+	assert.Equal(t, table.Table, "service")
+	assert.True(t, has)
+
+	_, has = table.Columns["id"]
+	assert.True(t, has)
+
+	price, has := table.Columns["计费方式"]
+	assert.True(t, has)
+	if has {
+		assert.True(t, price.Edit.Props["multiple"].(bool))
+	}
+
+	_, has = table.Filters["id"]
+	assert.True(t, has)
+
+	keywords, has := table.Filters["关键词"]
+	assert.True(t, has)
+	if has {
+		assert.True(t, keywords.Bind == "where.name.like")
+	}
 }
