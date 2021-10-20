5db230726ef3f7d5437b84c1df468f6c2cc7c24f
Max <shadow.iqka@qq.com>
2021-10-20 16:51:08 +0800

    fix link bug

--- spec.sh
+++ spec.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+repace() {
+    echo $1
+    sed -E "$1" ~/Code/yao/xiang-spec/README.md > ~/Code/yao/xiang-spec/README.new.md
+    rm ~/Code/yao/xiang-spec/README.md 
+    mv ~/Code/yao/xiang-spec/README.new.md ~/Code/yao/xiang-spec/README.md
+}
+
 make release
 VERSION=$(go run . version)
 rm -rf ../xiang-spec/xiang/*
@@ -6,8 +14,9 @@ cp  dist/release/xiang-* ../xiang-spec/xiang/
 rm -f ~/Code/bin/xiang
 ln -s ~/Code/yao/xiang-spec/xiang/xiang-$VERSION-darwin-amd64 ~/Code/bin/xiang
 
-# 更新 README.md 中的版本号
-sed "s/[ v-][0-9]\.[0-9]\.[0-9]/$VERSION/g" ~/Code/yao/xiang-spec/README.md > ~/Code/yao/xiang-spec/README.new.md
-rm ~/Code/yao/xiang-spec/README.md 
-mv ~/Code/yao/xiang-spec/README.new.md ~/Code/yao/xiang-spec/README.md
+# 更新 README.md 中的版本
+repace "s/\[[0-9]+\.[0-9]+\.[0-9]+\]/[$VERSION]/g"
+repace "s/\-[0-9]+\.[0-9]+\.[0-9]+\-/-$VERSION-/g"
+repace "s/Version\:[0-9]+\.[0-9]+\.[0-9]+/Version:$VERSION/g"
+
 make clean
