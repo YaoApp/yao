224245d88cd75fa4012146245b846cd001c5618f
Max <shadow.iqka@qq.com>
2021-10-25 01:10:17 +0800

    fix range bug

--- helper/range.go
+++ helper/range.go
@@ -26,37 +26,39 @@ func Range(v interface{}, process Process) {
 	switch value.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		For(0, any.Of(v).CInt(), process)
-		break
+		return
 	case reflect.Array, reflect.Slice:
 		data, err := jsoniter.Marshal(v)
 		if err != nil {
 			exception.New("数值格式不能使用Range %s", 400, err.Error()).Throw()
 		}
 		v := []interface{}{}
-		err = jsoniter.Unmarshal(data, v)
+		err = jsoniter.Unmarshal(data, &v)
 		rangeArray(v, process)
-		break
+		return
 	case reflect.String:
-		break
+		return
 	case reflect.Map:
 		data, err := jsoniter.Marshal(v)
 		if err != nil {
 			exception.New("数值格式不能使用Range %s", 400, err.Error()).Throw()
 		}
 		v := map[string]interface{}{}
-		err = jsoniter.Unmarshal(data, v)
+		err = jsoniter.Unmarshal(data, &v)
 		rangeMap(v, process)
-		break
+		return
 	case reflect.Struct:
 		data, err := jsoniter.Marshal(v)
 		if err != nil {
 			exception.New("数值格式不能使用Range %s", 400, err.Error()).Throw()
 		}
 		v := map[string]interface{}{}
-		err = jsoniter.Unmarshal(data, v)
+		err = jsoniter.Unmarshal(data, &v)
 		rangeMap(v, process)
-		break
+		return
 	}
+
+	exception.New("数值格式不能使用Range", 400).Ctx([]interface{}{v, value.Kind()}).Throw()
 }
 
 // For 过程控制
@@ -83,8 +85,8 @@ func rangeString(v string, p Process) {
 	var bytes = []byte(v)
 	for key, value := range bytes {
 		bindings := map[string]interface{}{
-			"::key":   key,
-			"::value": value,
+			"key":   key,
+			"value": value,
 		}
 		args := bindArgs(p.Args, bindings)
 		gou.NewProcess(p.Process, args...).Run()
@@ -95,8 +97,8 @@ func rangeString(v string, p Process) {
 func rangeMap(v map[string]interface{}, p Process) {
 	for key, value := range v {
 		bindings := map[string]interface{}{
-			"::key":   key,
-			"::value": value,
+			"key":   key,
+			"value": value,
 		}
 		args := bindArgs(p.Args, bindings)
 		gou.NewProcess(p.Process, args...).Run()
@@ -106,8 +108,8 @@ func rangeMap(v map[string]interface{}, p Process) {
 func rangeArray(v []interface{}, p Process) {
 	for key, value := range v {
 		bindings := map[string]interface{}{
-			"::key":   key,
-			"::value": value,
+			"key":   key,
+			"value": value,
 		}
 		args := bindArgs(p.Args, bindings)
 		gou.NewProcess(p.Process, args...).Run()
