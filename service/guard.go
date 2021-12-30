3cb780c631e583301fb377186799dc7ee90d7030
Max <shadow.iqka@qq.com>
2021-12-30 15:15:13 +0800

    + 跨域访问

--- service/guard.go
+++ service/guard.go
@@ -13,7 +13,8 @@ import (
 
 // Guards 服务中间件
 var Guards = map[string]gin.HandlerFunc{
-	"bearer-jwt": bearerJWT, // JWT 鉴权
+	"bearer-jwt":   bearerJWT,   // JWT 鉴权
+	"cross-domain": crossDomain, // 跨域许可
 }
 
 // JWT 鉴权
@@ -50,4 +51,19 @@ func bearerJWT(c *gin.Context) {
 	c.JSON(403, gin.H{"code": 403, "message": "无权访问该页面"})
 	c.Abort()
 	return
+}
+
+// crossDomain 跨域访问
+func crossDomain(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.Next()
 }
