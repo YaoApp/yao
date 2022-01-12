258457e2bd16ac4ce1f5e1799a39b9082caeda5b
Max <shadow.iqka@qq.com>
2022-01-12 22:27:52 +0800

    更新 发布脚本

--- release.sh
+++ release.sh
@@ -8,9 +8,9 @@ repace() {
 VERSION=$(go run . version)
 ssh max@demo-crm.iqka.com mkdir -p /data/demo-crm/ui/releases/$VERSION/
 ssh max@demo-crm.iqka.com "echo {\\\"version\\\":\\\"$VERSION\\\"} > /data/demo-crm/ui/releases/latest.json"
-scp ../xiang-spec/xiang/xiang-$VERSION-linux-amd64 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-linux-amd64
-scp ../xiang-spec/xiang/xiang-$VERSION-darwin-amd64 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-darwin-amd64
-scp ../xiang-spec/xiang/xiang-$VERSION-windows-386 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-windows-386
+scp ~/Code/bin/xiang-$VERSION-linux-amd64 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-linux-amd64
+scp ~/Code/bin/xiang-$VERSION-darwin-amd64 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-darwin-amd64
+scp ~/Code/bin/xiang-$VERSION-windows-386 max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/yao-$VERSION-windows-386
 
 repace "s/[0-9]+\.[0-9]+\.[0-9]+/$VERSION/g"
 scp ~/Code/yao/xiang/shell/install.sh.new max@demo-crm.iqka.com:/data/demo-crm/ui/releases/$VERSION/install.sh
