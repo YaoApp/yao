5cc005fbedf5325df1547ed430c394d09ab82948
Max <shadow.iqka@qq.com>
2022-01-31 11:26:40 +0800

    更新调试脚本

--- debug.sh
+++ debug.sh
@@ -5,6 +5,8 @@ VERSION=$(go run . version)
 rm -rf ../xiang-spec/xiang/*
 cp  dist/bin/xiang-* ../xiang-spec/xiang/
 rm -f ~/Code/bin/xiang
+rm -f ~/Code/bin/yao
 ln -s ~/Code/yao/xiang-spec/xiang/xiang-darwin-amd64 ~/Code/bin/xiang
+ln -s ~/Code/yao/xiang-spec/xiang/xiang-darwin-amd64 ~/Code/bin/yao
 
 make clean
