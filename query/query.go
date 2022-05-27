5548bc59dd87103d8f0254c363833233252946ac
Max <shadow.iqka@qq.com>
2022-05-27 19:22:04 +0800

    [add] Javascript Query object

--- query/query.go
+++ query/query.go
@@ -2,6 +2,7 @@ package query
 
 import (
 	"github.com/yaoapp/gou"
+	"github.com/yaoapp/gou/query"
 	dsl "github.com/yaoapp/gou/query/gou"
 	"github.com/yaoapp/kun/log"
 	"github.com/yaoapp/xun/capsule"
@@ -10,21 +11,21 @@ import (
 
 // Load 加载查询引擎
 func Load(cfg config.Config) {
-	XiangQuery()
+	DefaultQuery()
 }
 
-// XiangQuery 注册应用引擎象 xiang
-func XiangQuery() {
-	gou.RegisterEngine("xiang", &dsl.Query{
+// DefaultQuery register the default engine
+func DefaultQuery() {
+	query.Register("default", &dsl.Query{
 		Query: capsule.Query(),
 		GetTableName: func(s string) string {
 			if mod, has := gou.Models[s]; has {
 				return mod.MetaData.Table.Name
 			}
-			// exception.New("%s 数据模型尚未加载", 404).Throw()
 			log.Error("%s model does not load", s)
 			return s
 		},
 		AESKey: config.Conf.DB.AESKey,
 	})
+	query.Alias("default", "xiang")
 }
