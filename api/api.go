ef75f4cc4ad7fbd39d81c4456c9877b78cfcfc7a
Max <shadow.iqka@qq.com>
2022-07-16 22:26:41 +0800

    [change] websocket -> websocket server

--- api/api.go
+++ api/api.go
@@ -36,7 +36,7 @@ func LoadFrom(dir string, prefix string) error {
 	err = share.Walk(dir, ".ws.json", func(root, filename string) {
 		name := prefix + share.SpecName(root, filename)
 		content := share.ReadFile(filename)
-		_, err := gou.LoadWebSocket(string(content), name)
+		_, err := gou.LoadWebSocketServer(string(content), name)
 		if err != nil {
 			log.With(log.F{"root": root, "file": filename}).Error(err.Error())
 		}
