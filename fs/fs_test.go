a9bdf8e7f68ab4ef462ba661f0321ba007280579
Max <shadow.iqka@qq.com>
2022-10-12 22:11:45 +0800

    [add] load dsl & script filesystem

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -26,5 +26,39 @@ func TestLoad(t *testing.T) {
 	assert.Equal(t, int64(2), info.Size())
 
 	err = fs.Remove(data, "test.file")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDSL(t *testing.T) {
+	Load(config.Conf)
+	dsl := fs.MustGet("dsl")
+	name := filepath.Join("models", "test.mod.json")
+	_, err := fs.WriteFile(dsl, name, []byte(`{"foo": "bar", "hello":{ "int": 1, "float": 0.618}}`), 0644)
 	assert.Nil(t, err)
+
+	info, err := os.Stat(filepath.Join(config.Conf.Root, name))
+	assert.Nil(t, err)
+	assert.Equal(t, int64(69), info.Size())
+	dsl.Remove(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestScirpt(t *testing.T) {
+	Load(config.Conf)
+	script := fs.MustGet("script")
+	name := "test.js"
+	_, err := fs.WriteFile(script, name, []byte(`console.log("hello")`), 0644)
+	assert.Nil(t, err)
+
+	info, err := os.Stat(filepath.Join(config.Conf.Root, "scripts", name))
+	assert.Nil(t, err)
+	assert.Equal(t, int64(20), info.Size())
+	script.Remove(name)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
