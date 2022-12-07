91ba4be66df0ae716c45cbfade53b7fede588435
Max <shadow.iqka@qq.com>
2022-12-07 18:32:02 +0800

    [add] utils.tree.Flatten process

---
+++ utils/tree/tree.go
@@ -0,0 +1,52 @@
+package tree
+
+import (
+	"fmt"
+
+	"github.com/yaoapp/gou"
+)
+
+// ProcessFlatten utils.tree.Flatten cast to array
+func ProcessFlatten(process *gou.Process) interface{} {
+	process.ValidateArgNums(1)
+	array := process.ArgsArray(0)
+	option := process.ArgsMap(1, map[string]interface{}{"primary": "id", "children": "children", "parent": "parent"})
+	if _, has := option["primary"]; !has {
+		option["primary"] = "id"
+	}
+
+	if _, has := option["children"]; !has {
+		option["children"] = "children"
+	}
+
+	if _, has := option["parent"]; !has {
+		option["parent"] = "parent"
+	}
+
+	return flatten(array, option, nil)
+}
+
+func flatten(array []interface{}, option map[string]interface{}, id interface{}) []interface{} {
+
+	parent := fmt.Sprintf("%v", option["parent"])
+	primary := fmt.Sprintf("%v", option["primary"])
+	childrenField := fmt.Sprintf("%v", option["children"])
+	res := []interface{}{}
+	for _, v := range array {
+		row, ok := v.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		row[parent] = id
+		children, ok := row[childrenField].([]interface{})
+		delete(row, childrenField)
+		res = append(res, row)
+
+		if ok {
+			res = append(res, flatten(children, option, row[primary])...)
+
+		}
+	}
+	return res
+}
