a9e865ff413a0bd4aeb3556daf7903ac177bfb1c
Max <shadow.iqka@qq.com>
2023-02-03 15:21:31 +0800

    [add] migrate store

--- store/store.go
+++ store/store.go
@@ -11,26 +11,11 @@ import (
 func Load(cfg config.Config) error {
 	exts := []string{"*.yao", "*.json", "*.jsonc"}
 	return application.App.Walk("stores", func(root, file string, isdir bool) error {
+		if isdir {
+			return nil
+		}
+
 		_, err := store.Load(file, share.ID(root, file))
 		return err
 	}, exts...)
 }
-
-// // LoadFrom load from dir
-// func LoadFrom(dir string, prefix string) error {
-
-// 	if share.DirNotExists(dir) {
-// 		return fmt.Errorf("%s does not exists", dir)
-// 	}
-
-// 	err := share.Walk(dir, ".json", func(root, filename string) {
-// 		name := prefix + share.SpecName(root, filename)
-// 		content := share.ReadFile(filename)
-// 		_, err := gou.LoadStore(string(content), name)
-// 		if err != nil {
-// 			log.With(log.F{"root": root, "file": filename}).Error(err.Error())
-// 		}
-// 	})
-
-// 	return err
-// }
