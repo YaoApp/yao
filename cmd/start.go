c07b903c5c2056b36b1a2d70ea103cfa51624ea1
Max <shadow.iqka@qq.com>
2023-12-28 21:17:33 +0800

    Set runtime mode and display it in start command

--- cmd/start.go
+++ cmd/start.go
@@ -89,10 +89,15 @@ var startCmd = &cobra.Command{
 		mode := config.Conf.Mode
 		host := config.Conf.Host
 		dataRoot := fs.Root()
+		runtimeMode := config.Conf.Runtime.Mode
 
-		fmt.Println(color.WhiteString("\n---------------------------------"))
-		fmt.Println(color.WhiteString(strings.TrimPrefix(share.App.Name, "::")), color.WhiteString(share.App.Version), mode)
-		fmt.Println(color.WhiteString("---------------------------------"))
+		fmt.Println(color.WhiteString("\n--------------------------------------------"))
+		fmt.Println(
+			color.WhiteString(strings.TrimPrefix(share.App.Name, "::")),
+			color.WhiteString(share.App.Version),
+			mode,
+		)
+		fmt.Println(color.WhiteString("--------------------------------------------"))
 		if !share.BUILDIN {
 			root, _ := filepath.Abs(config.Conf.Root)
 			fmt.Println(color.WhiteString(L("Root")), color.GreenString(" %s", root))
@@ -104,6 +109,7 @@ var startCmd = &cobra.Command{
 			urls, _ = setup.URLs(config.Conf)
 		}
 
+		fmt.Println(color.WhiteString(L("Runtime")), color.GreenString(" %s", runtimeMode))
 		fmt.Println(color.WhiteString(L("Data")), color.GreenString(" %s", dataRoot))
 		fmt.Println(color.WhiteString(L("Listening")), color.GreenString(" %s:%d", config.Conf.Host, config.Conf.Port))
 		for _, url := range urls {
