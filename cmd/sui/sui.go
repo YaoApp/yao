ff6629270e02856a56159eb3c15ec558a20b07ca
Max <shadow.iqka@qq.com>
2024-02-08 12:17:21 +0800

    Add data flag to BuildCmd

--- cmd/sui/sui.go
+++ cmd/sui/sui.go
@@ -2,4 +2,5 @@ package sui
 
 func init() {
 	WatchCmd.PersistentFlags().StringVarP(&data, "data", "d", "::{}", L("Session Data"))
+	BuildCmd.PersistentFlags().StringVarP(&data, "data", "d", "::{}", L("Session Data"))
 }
