edad2e76f33ee6c399078803fa0b93d0207ae7f1
Max <shadow.iqka@qq.com>
2024-04-02 12:29:27 +0800

    Refactor bind logic in DSL

--- widgets/list/bind.go
+++ widgets/list/bind.go
@@ -10,23 +10,27 @@ import (
 // Bind model / store / table / ...
 func (dsl *DSL) Bind() error {
 
-	if dsl.Action.Bind == nil {
-		return nil
-	}
-
-	if dsl.Action.Bind.Model != "" {
-		return dsl.bindModel()
-	}
-
-	if dsl.Action.Bind.Store != "" {
-		return dsl.bindStore()
-	}
-
-	if dsl.Action.Bind.Table != "" {
-		return dsl.bindTable()
-	}
+	// Support bind in future version
 
 	return nil
+
+	// if dsl.Action.Bind == nil {
+	// 	return nil
+	// }
+
+	// if dsl.Action.Bind.Model != "" {
+	// 	return dsl.bindModel()
+	// }
+
+	// if dsl.Action.Bind.Store != "" {
+	// 	return dsl.bindStore()
+	// }
+
+	// if dsl.Action.Bind.Table != "" {
+	// 	return dsl.bindTable()
+	// }
+
+	// return nil
 }
 
 func (dsl *DSL) bindModel() error {
