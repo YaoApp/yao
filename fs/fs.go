00f10cd6302e2382a4d0d4fd6673385428c0d871
Max <shadow.iqka@qq.com>
2024-06-25 17:08:54 +0800

    [add] fs allows access to the app root directory ( it's an dangerous operation, be careful to use it)

--- fs/fs.go
+++ fs/fs.go
@@ -15,8 +15,11 @@ func Load(cfg config.Config) error {
 	scriptRoot := filepath.Join(cfg.AppSource, "scripts")
 	dslDenyList := []string{scriptRoot, cfg.DataRoot}
 
-	fs.Register("system", system.New(cfg.DataRoot))
+	fs.Register("system", system.New(cfg.DataRoot))                        // alias Data
 	fs.RootRegister("dsl", dsl.New(cfg.AppSource).DenyAbs(dslDenyList...)) // DSL
 	fs.RootRegister("script", system.New(scriptRoot))                      // Script
+
+	fs.Register("app", system.New(cfg.AppSource)) // App Soruce root path, it's an dangerous operation, be careful to use it.
+	fs.Register("data", system.New(cfg.DataRoot)) // Data root
 	return nil
 }
