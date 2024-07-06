57e72a698d7a4d6f38286c6dfdf38b0e6c283d54
Max <shadow.iqka@qq.com>
2024-07-07 00:51:07 +0800

    refactor: Copy slots in BuildAsComponent method

--- sui/core/types.go
+++ sui/core/types.go
@@ -47,7 +47,7 @@ type Page struct {
 
 // BuildContext is the struct for the build context
 type BuildContext struct {
-	components    map[string]bool
+	components    map[string]string
 	jitComponents map[string]bool
 	sequence      int
 	doc           *goquery.Document
