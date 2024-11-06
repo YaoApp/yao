c5a5a8432f583105ecb31d660a06470dc0daff9e
Max <shadow.iqka@qq.com>
2024-11-06 09:25:29 +0800

    Remove deprecated import and storage API loading from the codebase

--- api/api.go
+++ api/api.go
@@ -2,13 +2,11 @@ package api
 
 import (
 	"fmt"
-	"path/filepath"
 	"strings"
 
 	"github.com/yaoapp/gou/api"
 	"github.com/yaoapp/gou/application"
 	"github.com/yaoapp/yao/config"
-	"github.com/yaoapp/yao/data"
 	"github.com/yaoapp/yao/share"
 )
 
@@ -27,23 +25,6 @@ func Load(cfg config.Config) error {
 		}
 		return err
 	}, exts...)
-
-	// Load APIs from bindata (**will be removed in the future**)
-	names := []string{"import", "storage"}
-	for _, name := range names {
-		file := filepath.Join("yao", "apis", fmt.Sprintf("%s.http.json", name))
-		id := fmt.Sprintf("xiang.%s", name)
-
-		source, err := data.Read(file)
-		if err != nil {
-			messages = append(messages, err.Error())
-		}
-
-		_, err = api.LoadSource(file, source, id)
-		if err != nil {
-			messages = append(messages, err.Error())
-		}
-	}
 
 	if len(messages) > 0 {
 		return fmt.Errorf(strings.Join(messages, ";\n"))
