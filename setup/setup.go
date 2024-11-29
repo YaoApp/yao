6b6edab97c1fb812124298be81ffb685df3574aa
Max <shadow.iqka@qq.com>
2024-11-29 20:43:29 +0800

    Refactor network handling and endpoint retrieval; deprecate unused commands in startup

--- setup/setup.go
+++ setup/setup.go
@@ -9,19 +9,23 @@ import (
 	"github.com/yaoapp/yao/config"
 )
 
-// URLs get admin url
-func URLs(cfg config.Config) ([]string, error) {
-
-	ips, err := Ips()
+// Endpoints get endpoints
+func Endpoints(cfg config.Config) ([]Endpoint, error) {
+	networks, err := getNetworks()
 	if err != nil {
 		return nil, err
 	}
 
-	for i := range ips {
-		ips[i] = fmt.Sprintf("http://%s:%d", ips[i], cfg.Port)
+	var endpoints []Endpoint
+	for _, network := range networks {
+		endpoint := Endpoint{
+			URL:       fmt.Sprintf("http://%s:%d", network.IPv4, cfg.Port),
+			Interface: network.Interface,
+		}
+		endpoints = append(endpoints, endpoint)
 	}
 
-	return ips, nil
+	return endpoints, nil
 }
 
 func printError(message string, args ...interface{}) {
@@ -33,21 +37,49 @@ func printInfo(message string, args ...interface{}) {
 	fmt.Println(color.GreenString(message, args...))
 }
 
-// Ips get the local ip list
-func Ips() ([]string, error) {
-	addrs, err := net.InterfaceAddrs()
+func getNetworks() ([]Network, error) {
+	interfaces, err := net.Interfaces()
 	if err != nil {
 		return nil, err
 	}
 
-	iplist := []string{"127.0.0.1"}
-	for _, address := range addrs {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				iplist = append(iplist, ipnet.IP.String())
+	var networks []Network
+	for _, iface := range interfaces {
+		// 跳过 loopback 接口（如 lo0）
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		// 获取每个接口的地址信息
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		// 过滤只获取 IPv4 地址
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && ipnet.IP.To4() != nil {
+				// 将网卡名称和 IPv4 地址存储到 Network 结构体中
+				network := Network{
+					IPv4:      ipnet.IP.String(),
+					Interface: iface.Name,
+				}
+				// 添加到结果切片
+				networks = append(networks, network)
 			}
 		}
 	}
-	return iplist, nil
+	return networks, nil
+}
+
+// Network network
+type Network struct {
+	IPv4      string
+	Interface string
+}
+
+// Endpoint endpoint
+type Endpoint struct {
+	URL       string
+	Interface string
 }
